5255ddf368be0cdd1176c1ed3d91375d4069a4f5
fifau <fifaultimateamfifa15@gmail.com>
2023-05-11 23:59:06 +0700

    Added chat page with design

--- views/templates/chat.php
+++ views/templates/chat.php
@@ -1,5 +1,22 @@
 <main>
     <div class="chat-flex">
-
+        <div class="chat-flex-window">
+            <div class="chat-flex-dialog">
+                <div class="bot-message">
+                    <p>Lorem apsdasdjklhjaslkd asdfalkjshdfkl asdlfkjhasdjflk</p>
+                </div>
+                <div class="message">
+                    <p>Lorem apsdasdjklhjaslkd asdfalkjshdfkl asdlfkjhasdjflk</p>
+                </div>
+            </div>
+            <div class="chat-flex-input">
+                <form>
+                    <label class="chat-flex-input-label">
+                        <input class="input-message" type="text">
+                        <input class="input-submit-message" type="submit" value="&#10150;">
+                    </label>
+                </form>
+            </div>
+        </div>
     </div>
 </main>
